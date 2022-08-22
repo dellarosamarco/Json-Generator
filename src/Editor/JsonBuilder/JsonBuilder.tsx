e89291a3b4a29360f0b3c8c2d488ecd3b465f14f
dellarosamarco <dellarosamarco00@gmail.com>
2022-08-22 20:39:54 +0200

    update: json builder

--- src/Editor/JsonBuilder/JsonBuilder.tsx
+++ src/Editor/JsonBuilder/JsonBuilder.tsx
@@ -1,10 +1,30 @@
 import "./JsonBuilder.scss";
 
 const prefix = "json-builder";
+const rowPrefix = prefix + "-row";
+const cellPrefix = rowPrefix + "__cell";
+const cellTitlePrefix = cellPrefix + "-title";
 
 export default function JsonBuilder(){
     return (
         <div className={prefix}>
+            <div className={rowPrefix + " " + prefix + "-header"}>
+                <div className={cellPrefix}>
+                    <h1 className={cellTitlePrefix}>FIELD NAME</h1>
+                </div>
+
+                <div className={cellPrefix}>
+                    <h1 className={cellTitlePrefix}>TYPE</h1>
+                </div>
+
+                <div className={cellPrefix}>
+                    <h1 className={cellTitlePrefix}>GENERATION TYPE</h1>
+                </div>
+
+                <div className={cellPrefix}>
+                    <h1 className={cellTitlePrefix}>VALUE</h1>
+                </div>
+            </div>
         </div>
     );
 }
