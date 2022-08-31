d25832f0ce468a9f536f0e589ff19502b412e7fb
dellarosamarco <dellarosamarco00@gmail.com>
2022-08-31 13:59:17 +0200

    refactor: json builder row

--- src/Editor/JsonBuilder/JsonBuilderField/JsonBuilderField.tsx
+++ src/Editor/JsonBuilder/JsonBuilderField/JsonBuilderField.tsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { Field } from "../../../interfaces/Field.interface";
-import { FieldType, FieldTypeInterface, fieldTypes } from "../../../utilities/FieldType.model";
-import { GenerationType, GenerationTypeInterface, getGenerationType, haveOptions } from "../../../utilities/generationType.utilities";
+import { FieldType } from "../../../utilities/FieldType.model";
+import { GenerationType } from "../../../utilities/generationType.utilities";
 import { randomString } from "../../../utilities/randomString.method";
 import JsonBuilderHeader from "./JsonBuilderHeader/JsonBuilderHeader";
+import JsonBuilderRow from "./JsonBuilderRow/JsonBuilderRow";
 import OptionModal from "./OptionModal/OptionModal";
 
 interface JsonBuilderFieldProps{
@@ -13,44 +14,19 @@ interface JsonBuilderFieldProps{
 
 export default function JsonBuilderField(props : JsonBuilderFieldProps){
 
+    const rowPrefix = props.isHeader ? "json-builder-row__header" : "json-builder-row__field";
+
     const [state, forceState] = useState(false);
 
     const [showOptionModal, setShowOptionModal] = useState(false);
 
-    const isArrayOrObject = () => {
-        return (props.field!.type === FieldType.ARRAY || props.field!.type === FieldType.OBJECT);
-    }
-
-    function onEditFieldName(event : any){
-        props.field!.fieldName = event.target.value;
-        forceState(!state);
-    }
-
-    function onEditFieldValue(event : any){
-        props.field!.value = event.target.value;
-        forceState(!state);
-    }
-
-    function onEditFieldType(event : any){
-
-        if(event.target.value === "Array" || event.target.value === "Object"){
-            if(props.field!.children === undefined){
-                props.field!.children = [];
-            }
-        }
-
-        props.field!.type = event.target.value;
-        forceState(!state);
-    }
-
-    function onEditFieldGenerationType(event : any){
-        props.field!.generationType = event.target.value;
+    function updateState(){
         forceState(!state);
     }
 
     function onToggleObject(){
         props.field!.fieldOpened = props.field?.fieldOpened ? false : true;
-        forceState(!state);
+        updateState();
     }
 
     function onAddField(){
@@ -66,111 +42,11 @@ export default function JsonBuilderField(props : JsonBuilderFieldProps){
             options : {}
         } as Field);
 
-        forceState(!state);
+        updateState();
     }
 
     function onToggleOptionsModal(){
         setShowOptionModal(!showOptionModal);
-    }
-
-    const rowPrefix = props.isHeader ? "json-builder-row__header" : "json-builder-row__field";
-    const cellPrefix = rowPrefix + "__cell";
-    const cellTitlePrefix = cellPrefix + "-title";
-
-    function renderRows(){
-
-        function renderFieldNameCell(){
-            return (
-                <div className={cellPrefix}>
-                    <input value={props.field!.fieldName} onChange={(e) => onEditFieldName(e)}></input>
-                </div>
-            );
-        }
-
-        function renderFieldTypeCell(){
-            return (
-                <div className={cellPrefix}>
-                    <select onChange={(e) => onEditFieldType(e)} defaultValue={props.field?.type}>
-                        {
-                            fieldTypes.map((fieldType : FieldTypeInterface) => {
-                                return (
-                                    <option key={fieldType.fieldType} value={fieldType.fieldType}>{fieldType.fieldName}</option>
-                                )
-                            })
-                        }
-                    </select>
-                </div>
-            );
-        }
-
-        function renderFieldGenerationTypeCell(){
-            return(
-                <div className={cellPrefix}>
-                    {
-                        (!isArrayOrObject()) ? (
-                            <select onChange={(e) => onEditFieldGenerationType(e)} defaultValue={props.field?.generationType}>
-                                {
-                                    getGenerationType(props.field?.type!).map((generationTypeInterface : GenerationTypeInterface) => {
-                                        return (
-                                            <option key={generationTypeInterface.name} value={generationTypeInterface.type}>{generationTypeInterface.name}</option>
-                                        )
-                                    })
-                                }
-                            </select>
-                        ) : <></>
-                    }
-                </div>
-            );
-        }
-
-        function renderFieldValueCell(){
-            function render(){
-                return props.field?.generationType === GenerationType.CUSTOM_VALUE ? 
-                <input value={props.field!.value} onChange={(e) => onEditFieldValue(e)}></input> : 
-                <></>
-            }
-
-            return(
-                <div className={cellPrefix}>
-                    {
-                        (!isArrayOrObject()) ? 
-                        (render()) 
-                        : <button className="json-builder-row-button" onClick={onToggleObject}>{ props.field?.fieldOpened ? 'CLOSE OBJECT' : 'OPEN OBJECT'}</button>
-                    }
-                </div>
-            );
-        }
-
-        function renderOptionsCell(){
-            if(isArrayOrObject()) return <div className={cellPrefix}></div>;
-
-            return(
-                <div className={cellPrefix}>
-                    {
-                        haveOptions(props.field?.generationType!) ? 
-                        <button className="json-builder-row-button" onClick={onToggleOptionsModal}>SET OPTIONS</button> :
-                        <></>
-                    }
-                </div>
-            );
-        }
-
-        function renderOptionsModal(){
-            return <></>;
-        }
-
-        return (
-            (
-                <>
-                    {renderFieldNameCell()}
-                    {renderFieldTypeCell()}
-                    {renderFieldGenerationTypeCell()}
-                    {renderFieldValueCell()}
-                    {renderOptionsCell()}
-                    {renderOptionsModal()}
-                </>
-            )
-        )
     }
 
     function renderChildren(){
@@ -195,12 +71,28 @@ export default function JsonBuilderField(props : JsonBuilderFieldProps){
     return (
         <>
             <div className={"json-builder-row " + rowPrefix}>
-                { props.isHeader ? <JsonBuilderHeader></JsonBuilderHeader> : renderRows() }
+                { 
+                    props.isHeader ? 
+                        <JsonBuilderHeader></JsonBuilderHeader> : 
+                        <JsonBuilderRow 
+                            field={props.field} 
+                            updateState={updateState} 
+                            onToggleObject={onToggleObject} 
+                            onToggleOptionsModal={onToggleOptionsModal} 
+                        /> 
+                }
             </div>
 
-            <OptionModal show={showOptionModal} showToggler={() => onToggleOptionsModal} field={props.field!}></OptionModal>
+            <OptionModal 
+                show={showOptionModal} 
+                showToggler={() => onToggleOptionsModal} 
+                field={props.field!} 
+            />
 
-            { !props.isHeader && isArrayOrObject() ? renderChildren() : <></> }
+            { 
+                !props.isHeader && (props.field!.type === FieldType.ARRAY || props.field!.type === FieldType.OBJECT) ? 
+                renderChildren() : <></> 
+            }
         </>
     );
 }
---
+++ src/Editor/JsonBuilder/JsonBuilderField/JsonBuilderRow/JsonBuilderRow.tsx
@@ -0,0 +1,137 @@
+import { FieldType, FieldTypeInterface, fieldTypes } from "../../../../utilities/FieldType.model";
+import { GenerationType, GenerationTypeInterface, getGenerationType, haveOptions } from "../../../../utilities/generationType.utilities";
+
+export default function JsonBuilderRow(props:any){
+
+    const rowPrefix = props.isHeader ? "json-builder-row__header" : "json-builder-row__field";
+    const cellPrefix = rowPrefix + "__cell";
+    const cellTitlePrefix = cellPrefix + "-title";
+
+    const isArrayOrObject = () => {
+        return (props.field!.type === FieldType.ARRAY || props.field!.type === FieldType.OBJECT);
+    }
+
+    function onEditFieldName(event : any){
+        props.field!.fieldName = event.target.value;
+        props.updateState();
+    }
+
+    function onEditFieldValue(event : any){
+        props.field!.value = event.target.value;
+        props.updateState();
+    }
+
+    function onEditFieldType(event : any){
+
+        if(event.target.value === "Array" || event.target.value === "Object"){
+            if(props.field!.children === undefined){
+                props.field!.children = [];
+            }
+        }
+
+        props.field!.type = event.target.value;
+        props.updateState();
+    }
+
+    function onEditFieldGenerationType(event : any){
+        props.field!.generationType = event.target.value;
+        props.updateState();
+    }
+
+    function renderRows(){
+        function renderFieldNameCell(){
+            return (
+                <div className={cellPrefix}>
+                    <input value={props.field!.fieldName} onChange={(e) => onEditFieldName(e)}></input>
+                </div>
+            );
+        }
+
+        function renderFieldTypeCell(){
+            return (
+                <div className={cellPrefix}>
+                    <select onChange={(e) => onEditFieldType(e)} defaultValue={props.field?.type}>
+                        {
+                            fieldTypes.map((fieldType : FieldTypeInterface) => {
+                                return (
+                                    <option key={fieldType.fieldType} value={fieldType.fieldType}>{fieldType.fieldName}</option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
+            );
+        }
+
+        function renderFieldGenerationTypeCell(){
+            return(
+                <div className={cellPrefix}>
+                    {
+                        (!isArrayOrObject()) ? (
+                            <select onChange={(e) => onEditFieldGenerationType(e)} defaultValue={props.field?.generationType}>
+                                {
+                                    getGenerationType(props.field?.type!).map((generationTypeInterface : GenerationTypeInterface) => {
+                                        return (
+                                            <option key={generationTypeInterface.name} value={generationTypeInterface.type}>{generationTypeInterface.name}</option>
+                                        )
+                                    })
+                                }
+                            </select>
+                        ) : <></>
+                    }
+                </div>
+            );
+        }
+
+        function renderFieldValueCell(){
+            function render(){
+                return props.field?.generationType === GenerationType.CUSTOM_VALUE ? 
+                <input value={props.field!.value} onChange={(e) => onEditFieldValue(e)}></input> : 
+                <></>
+            }
+
+            return(
+                <div className={cellPrefix}>
+                    {
+                        (!isArrayOrObject()) ? 
+                        (render()) 
+                        : <button className="json-builder-row-button" onClick={props.onToggleObject}>{ props.field?.fieldOpened ? 'CLOSE OBJECT' : 'OPEN OBJECT'}</button>
+                    }
+                </div>
+            );
+        }
+
+        function renderOptionsCell(){
+            if(isArrayOrObject()) return <div className={cellPrefix}></div>;
+
+            return(
+                <div className={cellPrefix}>
+                    {
+                        haveOptions(props.field?.generationType!) ? 
+                        <button className="json-builder-row-button" onClick={props.onToggleOptionsModal}>SET OPTIONS</button> :
+                        <></>
+                    }
+                </div>
+            );
+        }
+
+        function renderOptionsModal(){
+            return <></>;
+        }
+
+        return (
+            (
+                <>
+                    {renderFieldNameCell()}
+                    {renderFieldTypeCell()}
+                    {renderFieldGenerationTypeCell()}
+                    {renderFieldValueCell()}
+                    {renderOptionsCell()}
+                    {renderOptionsModal()}
+                </>
+            )
+        )
+    }
+
+    return renderRows();
+}
